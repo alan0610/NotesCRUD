26452e97653b5056c3779116bd1332ebc8ad3cf1
Alan Ades <alan.ades2@gmail.com>
2023-08-07 15:48:03 -0300

    fixing gh-pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
-import CompShow from './components/Show';
-import CompCreate from './components/Create'
-import CompUpdate from './components/Update'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import "./App.css";
+import CompShow from "./components/Show";
+import CompCreate from "./components/Create";
+import CompUpdate from "./components/Update";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/notesCRUD">
-        <Routes>
-          <Route path='/' element={ <CompShow />} />
-          <Route path='/create' element={ <CompCreate />} />
-          <Route path='/update/:id' element={ <CompUpdate />} />
-        </Routes>
-      </BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element={<CompShow />} />
+        <Route path="/create" element={<CompCreate />} />
+        <Route path="/update/:id" element={<CompUpdate />} />
+      </Routes>
     </div>
   );
 }
