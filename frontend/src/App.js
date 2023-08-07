ac2381be19c922b57200365080b83930ed64f431
Alan Ades <alan.ades2@gmail.com>
2023-08-07 15:40:33 -0300

    fixing browser-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/notesCRUD">
         <Routes>
           <Route path='/' element={ <CompShow />} />
           <Route path='/create' element={ <CompCreate />} />
